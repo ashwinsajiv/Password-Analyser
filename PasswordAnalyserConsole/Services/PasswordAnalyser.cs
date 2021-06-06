2de5f239c591e9b3960f43380e71321b2c158d12
Ashwin Sajiv <37055705+ashwinsajiv@users.noreply.github.com>
2021-06-06 23:50:11 +1000

    Formatting

--- PasswordAnalyserConsole/Services/PasswordAnalyser.cs
+++ PasswordAnalyserConsole/Services/PasswordAnalyser.cs
@@ -1,8 +1,14 @@
 using System;
+using Newtonsoft.Json;
 using RestSharp;
 
 namespace PasswordAnalyserConsole.Services
 {
+    public class ResponseObject
+    {
+        public string Strength { get; set; }
+        public int Breach { get; set; }
+    }
     public class PasswordAnalyser
     {
         private string _password;
@@ -26,10 +32,8 @@ namespace PasswordAnalyserConsole.Services
             string body = "\"" + _password + "\"";
             request.AddParameter("application/json", body, ParameterType.RequestBody);
             IRestResponse response = client.Execute(request);
-            var resp = response.Content.Split(',');
-            var strength = resp[0].Substring(13, resp[0].Length - 14);
-            var breach = Int32.Parse(resp[1].Substring(9, resp[1].Length - 10));
-            return (strength, breach);
+            var res = JsonConvert.DeserializeObject<ResponseObject>(response.Content);
+            return (res.Strength, res.Breach);
         }
     }
 }
